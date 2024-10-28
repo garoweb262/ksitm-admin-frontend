f3690ebc33c820e957c534431fc2d50f142ac061
garoweb262 <muhdgazzali01@gmail.com>
2024-10-28 16:19:01 +0100

    fixed

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
-  // baseURL: 'https://ksitm-backend.onrender.com/api/v1',
+  // baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://ksitm-backend.onrender.com/api/v1',
 });
 
 export default instance;
